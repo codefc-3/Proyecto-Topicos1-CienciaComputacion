b2336f3c4356404b578b502b07a76eeeacc2d064
konradtrejo <kbtch95@gmail.com>
2019-05-22 15:07:15 -0500

    desarrollado hasta Api de conexion

---
+++ warnApp/app/src/main/java/app/mycompany/warnapp/login.java
@@ -0,0 +1,23 @@
+package app.mycompany.warnapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+    public void register(View view){
+        Intent i = new Intent(this,Registro.class);
+        startActivity(i);
+    }
+    public void panel(View view){
+        Intent i = new Intent(this,MainActivity.class);
+        startActivity(i);
+    }
+}
