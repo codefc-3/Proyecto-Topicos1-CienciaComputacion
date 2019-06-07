4a5c40615dd6e218ee8850d43c2e3bb03d2f82cb
konradtrejo <kbtch95@gmail.com>
2019-06-07 03:15:33 -0500

    Marcadores en Maps

--- warnApp/app/src/main/java/app/mycompany/warnapp/MapsActivity.java
+++ warnApp/app/src/main/java/app/mycompany/warnapp/MapsActivity.java
@@ -1,12 +1,13 @@
 package app.mycompany.warnapp;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -39,8 +40,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng Lima = new LatLng(-12.02175347671593, -77.04777275897878);
+        mMap.addMarker(new MarkerOptions().position(Lima).title("Ubicacion Actual ").snippet("UNI").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Lima,13));
+
+
+        LatLng SMP = new LatLng(-12.0062974, -77.0587863);
+        mMap.addMarker(new MarkerOptions().position(SMP).title("Robo de celular").snippet(" Cerca a Plaza Norte 4pm").icon(BitmapDescriptorFactory.fromResource(R.drawable.referencia)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SMP,13));
+
+
+        LatLng Rimac = new LatLng(-12.024231066357245, -77.03881091175897);
+        mMap.addMarker(new MarkerOptions().position(Rimac).title("Robo con arma").snippet("Barristas Hinchas de Alianza Lima").icon(BitmapDescriptorFactory.fromResource(R.drawable.referencia)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Rimac,13));
+
+
+        LatLng Independencia = new LatLng(-12.009649513643595, -77.05076285985106);
+        mMap.addMarker(new MarkerOptions().position(Independencia).title("Robo de automovil").snippet("cerca a la plaza Metro UNI 3 pm").icon(BitmapDescriptorFactory.fromResource(R.drawable.referencia)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Independencia,13));
+
+
+        LatLng Bolognesi = new LatLng(-12.059667229549092, -77.04223751667847);
+        mMap.addMarker(new MarkerOptions().position(Bolognesi).title("Robo de cartera").snippet("cerca a la plaza Bolognesi 3 pm").icon(BitmapDescriptorFactory.fromResource(R.drawable.referencia)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Bolognesi,13));
+
+
+        LatLng Metro = new LatLng(-12.02850207920963, -77.04996739640971);
+        mMap.addMarker(new MarkerOptions().position(Metro).title("Raqueteros robaron a un grupo de Estudiantes").snippet("Cerca a caqueta 8 pm").icon(BitmapDescriptorFactory.fromResource(R.drawable.referencia)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Metro,13));
+
+
+
+        LatLng Victoria = new LatLng(-12.0739937, -77.0181966);
+        mMap.addMarker(new MarkerOptions().position(Victoria).title("Robo con arma blanca de pertenencias").snippet("Cuadra 6 avenida Mexico 10 pm").icon(BitmapDescriptorFactory.fromResource(R.drawable.referencia)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Victoria,13));
+
+
+
+
+        LatLng Tacna = new LatLng(-12.06381996662374, -77.03454367412584);
+        mMap.addMarker(new MarkerOptions().position(Tacna).title("Estafa de productos adquiridos").snippet(" Av 28 de julio cuadra 15 ").icon(BitmapDescriptorFactory.fromResource(R.drawable.referencia)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Tacna,13));
+
+        LatLng Miraflores = new LatLng(-12.1322691, -77.0301446);
+        mMap.addMarker(new MarkerOptions().position(Miraflores).title("Robo de celular y camara de Turista").snippet("cerca a LarcoMar 3 pm").icon(BitmapDescriptorFactory.fromResource(R.drawable.referencia)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Miraflores,13));
+
     }
 }
